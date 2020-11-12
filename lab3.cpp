e80e435f3eb92b037ba23db24b88504ba8794e3a
kmichalski1 <73621723+kmichalski1@users.noreply.github.com>
2020-11-12 12:40:42 +0100

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -11,13 +11,34 @@ public:
 
 class Kolo : Figura
 {
+private:
+    double r = 0;
+public:
+    Kolo(double r);
+    
+    Kolo::Kolo(double r)
+   {
+        pole = pi*r^2;
+   }
 };
 
 class Kwadrat : Figura
 {
+private:
+    double a = 0;
+public:
+    Kwadrat(double a);
+    
+    Kwadrat::Kwadrat(double a)
+   {
+        pole = a*a;
+   }
 };
 
 int main()
 {
-
+    Kolo ko1(3);
+    Kolo ko2(4);
+    Kwadrat kw1(3);
+    Kwadrat kw2(4);
 }
