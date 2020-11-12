7a8df5453b808193b9b677ee8874b9e0338ee2d8
kmichalski1 <73621723+kmichalski1@users.noreply.github.com>
2020-11-12 12:29:20 +0100

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+class Figura
+{
+private:
+    double pole = 0;
+public:
+    setFig();
+    getFig();
+};
+
+class Kolo : Figura
+{
+};
+
+class Kwadrat : Figura
+{
+};
+
 int main()
 {
-    puts("Hello, World!");
+
 }
